85034fc5e10a405b7281d67681a0e1abd0cb2471
Lahzey <arno.1999@hotmail.ch>
2023-04-17 16:42:01 +0200

    Fixed air drag precision issue and added overshoot protection

--- Assets/Scripts/VectorFieldPhysics/VectorPhysics.cs
+++ Assets/Scripts/VectorFieldPhysics/VectorPhysics.cs
@@ -6,12 +6,12 @@ namespace VectorFieldPhysics {
 public class VectorPhysics : MonoBehaviour {
 	
 	private static readonly Vector3 GRAVITY = new Vector3(0, -9.81f, 0);
-	private static readonly float AIR_DENSITY = 1.293f;
+	private static readonly double AIR_DENSITY = 1.293f;
 
 	[SerializeField] public List<MonoBehaviour> vectorFields = new List<MonoBehaviour>();
-	[SerializeField] public float dragCoefficient = 0.5f;
-	[SerializeField] public float weightKg = 1f;
-	[SerializeField] public float areaM2 = 1f;
+	[SerializeField] public double dragCoefficient = 0.5f;
+	[SerializeField] public double weightKg = 1f;
+	[SerializeField] public double areaM2 = 1f;
 	[SerializeField] public bool useGravity = true;
 	[SerializeField] public Vector3 velocity;
 
@@ -36,8 +36,9 @@ public class VectorPhysics : MonoBehaviour {
 		// air drag
 		Debug.DrawLine(transform.position, transform.position + airSpeed * 10000, Color.red);
 		Vector3 relativeVelocity = velocity - airSpeed;
-		Vector3 airDrag = 0.5f * AIR_DENSITY * relativeVelocity.sqrMagnitude * dragCoefficient * areaM2 * relativeVelocity.normalized;
-		velocity -= airDrag * (time / weightKg); // bracket should not matter (despite what rider thinks), but does improve performance
+		double airDragPercent = 0.5f * AIR_DENSITY * relativeVelocity.sqrMagnitude * dragCoefficient * areaM2 * time / weightKg;
+		if (airDragPercent > 1) airDragPercent = 1;
+		velocity -= (float) airDragPercent * relativeVelocity.normalized;
 		
 		// prevent really small numbers, just stop the object
 		if (velocity.x < 0.0001f && velocity.x > -0.0001f) velocity.x = 0;
