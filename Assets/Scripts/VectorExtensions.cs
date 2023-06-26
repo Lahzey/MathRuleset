b5501d7bea2965abd5e8e94f210f267bfd61d348
Lahzey <arno.1999@hotmail.ch>
2023-06-26 17:07:12 +0200

    Starting coding steering behaviour

---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace {
+public static class Vector3Extensions {
+	public static Vector2 To2D(this Vector3 self) {
+		return new Vector2(self.x, self.z);
+	}
+}
+
+public static class Vector2Extensions {
+	public static Vector3 To3D(this Vector2 self, float y = 0) {
+		return new Vector3(self.x, y, self.y);
+	}
+}
+}
